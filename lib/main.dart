826e41615cc9f8586e85f91f409fca37602f79e5
ProgrammerAuditore <victorvj098@gmail.com>
2022-05-29 22:44:27 -0500

    *** Wallet-App-Clone : Se agrego un proyecto de Wallet App Clone

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 import 'package:appfoodfast/nav_item/About/About.dart';
 import 'package:appfoodfast/nav_item/Home/Home.dart';
 import 'package:appfoodfast/nav_item/Sucursal/Sucursal.dart';
+import 'package:appfoodfast/screens/card_page.dart';
+import 'package:appfoodfast/screens/page_home.dart';
 import 'package:flutter/material.dart';
 import 'package:appfoodfast/config.dart';
 
@@ -14,10 +16,54 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: MyAppConfig.appColorPrimary,
       ),
-      home: MyPageMain()
+      home: WalletApp(),
     );
   }
 }
+
+class WalletApp extends StatefulWidget {
+  @override
+  _WalletAppState createState() => _WalletAppState();
+}
+
+class _WalletAppState extends State<WalletApp> {
+  var screens = [
+    HomePage(),
+    CardPage(),
+  ];
+
+  int selectedTab = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(38, 81, 158, 1),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.credit_card), label: 'Card'),
+        ],
+        onTap: (index) {
+          setState(() {
+            selectedTab = index;
+          });
+        },
+        showUnselectedLabels: true,
+        iconSize: 30,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        elevation: 0,
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body: screens[selectedTab],
+    );
+  }
+}
+
+// ==== TESTING
 
 class MyPageMain extends StatefulWidget {
   MyPageMain({Key? key}) : super(key: key);

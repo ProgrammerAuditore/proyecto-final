5c74d2926f87dd85e1623eee2a129b8b5988d226
ProgrammerAuditore <victorvj098@gmail.com>
2022-05-31 05:10:46 -0500

    *** dev : Creando pagina Profile

---
+++ lib/pages/Profile/ContentPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ContentPage extends StatelessWidget {
+  const ContentPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/pages/Profile/ProfilePage.dart
+++ lib/pages/Profile/ProfilePage.dart
@@ -1,33 +1,21 @@
-import 'package:appfoodfast/pages/Profile/componentes/Avatar.dart';
-import 'package:appfoodfast/pages/Profile/componentes/ProfileHeader.dart';
-import 'package:appfoodfast/pages/Profile/componentes/UserInfo.dart';
+import 'package:appfoodfast/components/PageContent.dart';
+import 'package:appfoodfast/pages/Profile/ContentPage.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: double.infinity,
+      child: Stack(
         children: <Widget>[
-          ProfileHeader(
-            avatar: NetworkImage('https://firebasestorage.googleapis.com/v0/b/dl-flutter-ui-challenges.appspot.com/o/img%2F6.jpg?alt=media'),
-            coverImage: NetworkImage('https://firebasestorage.googleapis.com/v0/b/dl-flutter-ui-challenges.appspot.com/o/img%2F6.jpg?alt=media'),
-            title: "Ramesh Mana",
-            subtitle: "Manager",
-            actions: <Widget>[
-              MaterialButton(
-                color: Colors.white,
-                shape: CircleBorder(),
-                elevation: 0,
-                child: Icon(Icons.edit),
-                onPressed: () {},
-              )
-            ],
+          Container(
+            child: Text("Page profile"),
           ),
-          const SizedBox(height: 10.0),
-          UserInfo(),
+          PageContent(
+            pageContent: ContentPage(),
+          )
         ],
       ),
     );
@@ -35,33 +23,8 @@ class ProfilePage extends StatelessWidget {
 
   BottomNavigationBarItem barItemProfile() {
     return const BottomNavigationBarItem(
-      icon: Icon(Icons.person_pin),
-      label: 'Profile',
+      icon: Icon(Icons.home),
+      label: 'Home',
     );
   }
 }
-
-class PNetworkImage extends StatelessWidget {
-  final String? image;
-  final BoxFit? fit;
-  final double? width, height;
-  const PNetworkImage(this.image, {Key? key, this.fit, this.height, this.width})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Image.network(
-      image!,
-      // placeholder: (context, url) => Center(child: CircularProgressIndicator()),
-      // errorWidget: (context, url, error) => Image.asset('assets/placeholder.jpg',fit: BoxFit.cover,),
-      fit: fit,
-      width: width,
-      height: height,
-    );
-  }
-}
-
-
-
-
-

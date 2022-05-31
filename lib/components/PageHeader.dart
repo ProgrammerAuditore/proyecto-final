663c759625d83b8ba3ac5548a737274126c983d9
ProgrammerAuditore <victorvj098@gmail.com>
2022-05-31 16:48:09 -0500

    *** dev : Se agrego foto de perfil

--- lib/components/PageHeader.dart
+++ lib/components/PageHeader.dart
@@ -37,10 +37,7 @@ class PageHeader extends StatelessWidget {
                       radius: 25,
                       backgroundColor: Colors.white,
                       child: ClipOval(
-                        child: Image.network(
-                          'https://firebasestorage.googleapis.com/v0/b/dl-flutter-ui-challenges.appspot.com/o/img%2F6.jpg?alt=media',
-                          fit: BoxFit.contain,
-                        ),
+                        child: Image(image: AssetImage('assets/profile.jpg')),
                       ),
                     )
                   ],
